fccba2ce8c075701f7b85c3f4e4d17bbbd639c86
OxheadChuck6o6o <34916687+OxheadChuck6o6o@users.noreply.github.com>
2025-01-17 12:36:08 +0000

    scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -375,7 +375,7 @@ def calculate_machine_utilization(df):
         return daily_utilization
 
     # Apply the function to calculate daily utilization for each task
-    st.session_state.dfm["Daily Utilization"] = st.session_state.dfm.apply(
+    dfm["Daily Utilization"] = apply(
         lambda row: calculate_daily_utilization(row["Start Time"], row["End Time"]),
         axis=1,
     )
