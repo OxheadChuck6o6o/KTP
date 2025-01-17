37503794148679dc195ce39ee58007678e87b8f3
OxheadChuck6o6o <34916687+OxheadChuck6o6o@users.noreply.github.com>
2025-01-17 12:41:33 +0000

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 # from scheduler import dfm  # Import the processed `dfm` from the backend
-from visualisation import visualisation  # Import Gantt chart visualization
+from visualisation import visualisation_tab  # Import Gantt chart visualization
 # from results import results
 from modify import modify_tab
 from product_list_change import product_list_change
