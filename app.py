0f72a139cfd23578631996a68bd176067572fe83
OxheadChuck6o6o <34916687+OxheadChuck6o6o@users.noreply.github.com>
2025-02-01 01:50:26 +0000

    app.py

--- app.py
+++ app.py
@@ -1,67 +1,70 @@
 import streamlit as st
-import pandas as pd
-# from scheduler import dfm  # Import the processed `dfm` from the backend
-from visualisation import visualisation_tab  # Import Gantt chart visualization
-# from results import results
-from modify import modify_tab
-from product_list_change import product_list_change
-from product_catalogue import product_catalogue
-from scheduler import df, dfm, product_waiting_df, component_waiting_df, late_df, initialise_state
 
-# Set page configuration
 st.set_page_config(
     page_title="Machine Production Scheduler",
     page_icon="🦾",
     layout="wide"
 )
 
-# Main Title
-st.title("Machine Production Scheduler")
+import pandas as pd
+from visualisation import visualisation_tab  # Import Gantt chart visualization
+from modify import modify_tab
+from product_list_change import product_list_change
+from product_catalogue import product_catalogue
+from scheduler import late_products, calculate_waiting_time, calculate_machine_utilization, adjust_end_time_and_start_time, schedule_production_with_days
 
-# st.write(st.session_state)
+# df, dfm, component_waiting_df, product_waiting_df, late_df
+df = pd.read_excel('Product Details_v1.xlsx', sheet_name='P')
 
-# if "df" not in st.session_state:
-#     st.session_state.dfm = df
-# if "dfm" not in st.session_state:
-#     st.session_state.dfm = dfm
-# if "product_waiting_df" not in st.session_state:
-#     st.session_state.product_waiting_df = product_waiting_df
-# if "component_waiting_df" not in st.session_state:
-#     st.session_state.dfm = component_waiting_df
-# if "late_df" not in st.session_state:
-#     st.session_state.dfm = late_df
+df['Order Processing Date'] = pd.to_datetime(df['Order Processing Date'])
+df['Promised Delivery Date'] = pd.to_datetime(df['Promised Delivery Date'])
+df['Start Time'] = pd.NaT  # Initialize as empty datetime
+df['End Time'] = pd.NaT  # Initialize as empty datetime
+df['status'] = ''  # Initialize the Status column
+df.loc[df['Process Type'] == 'In House', 'status'] = 'InProgress_In House'
+df.loc[df['Process Type'] == 'Outsource','status'] = 'InProgress_Outsource'
+df = df.sort_values(by=['Promised Delivery Date',
+                        'Product Name',
+                        'Components']).reset_index(drop=True)
 
-# df = st.session_state.df
-# dfm = st.session_state.dfm
-# product_waiting_df = st.session_state.product_waiting_df
-# component_waiting_df =  st.session_state.component_waiting_df 
-# late_df = st.session_state.dfm
+dfm = df.copy()
+dfm = schedule_production_with_days(dfm)
+# dfm = adjust_end_time_and_start_time(dfm)
+dfm = dfm.sort_values(by=['Start Time','End Time','Promised Delivery Date'])
+dfm['legend'] = dfm['Components']
+for i in range(len(dfm)):
+  if dfm['Machine Number'][i] == 'OutSrc':
+    dfm['legend'][i] = 'OutSrc'
+
+machine_utilization_df = calculate_machine_utilization(dfm.copy())
+
+component_waiting_df = calculate_waiting_time(
+        dfm,
+        group_by_column='Components',
+        date_columns=('Order Processing Date', 'Start Time'))
+
+product_waiting_df = calculate_waiting_time(
+        dfm,
+        group_by_column='Product Name',
+        date_columns=('Order Processing Date', 'Start Time'))
+
+late_df = late_products(dfm)
+
+def initialise_state():
+  if "df" not in st.session_state:
+    st.session_state.df = df
+  if "dfm" not in st.session_state:  # Adjust Start and End Times
+    st.session_state.dfm = dfm
+  if "machine_utilization_df" not in st.session_state:
+    st.session_state.machine_utilization_df = machine_utilization_df
+  if "component_waiting_df" not in st.session_state:
+    st.session_state.component_waiting_df = component_waiting_df
+  if "product_waiting_df" not in st.session_state:
+    st.session_state.product_waiting_df = product_waiting_df
+  if "late_df" not in st.session_state:
+    st.session_state.late_df = late_df
 
 initialise_state()
-
-# if "late_df" not in st.session_state:
-#     st.session_state.late_df = late_df
-# if "df" not in st.session_state:
-#     st.session_state.df = df
-# if "dfm" not in st.session_state:  # Adjust Start and End Times
-#     st.session_state.dfm = dfm
-# if "component_waiting_df" not in st.session_state:
-#     st.session_state.component_waiting_df = component_waiting_df
-# if "product_waiting_df" not in st.session_state:
-#     st.session_state.product_waiting_df = product_waiting_df
-
-# # File Download Button
-# @st.cache_data
-# def convert_df_to_excel(df):
-#     return df.to_excel(index=False).encode('utf-8')
-
-# csv_file = convert_df_to_excel(dfm)
-# st.download_button(
-#     label="📥 Download Current File",
-#     data=csv_file,
-#     file_name="Machine_Production_Schedule.csv",
-#     mime="text/csv"
-# )
 
 # Add Tabs Below
 tabs = st.tabs([
@@ -74,12 +77,9 @@ tabs = st.tabs([
     ])
 
 # Tab Content
-with tabs[0]:  # Visualisation Tab
-    # if "dfm" in st.session_state:
-        # dfm = st.session_state.dfm
+with tabs[0]:
     visualisation_tab()
 
-    
 with tabs[1]:
     modify_tab()
 
@@ -88,21 +88,3 @@ with tabs[2]:
 
 with tabs[3]:
     product_catalogue()
-
-# with tabs[4]:
-#     similarity()
-
-# with tabs[5]:
-#     results()
-
-
-# if "late_df" not in st.session_state:
-st.session_state.late_df = late_df
-# if "df" not in st.session_state:
-st.session_state.df = df
-# if "dfm" not in st.session_state:  # Adjust Start and End Times
-st.session_state.dfm = dfm
-# if "component_waiting_df" not in st.session_state:
-st.session_state.component_waiting_df = component_waiting_df
-# if "product_waiting_df" not in st.session_state:
-st.session_state.product_waiting_df = product_waiting_df
