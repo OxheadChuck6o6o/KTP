45967865cf0e120bdd406529f0583553119b8fa2
OxheadChuck6o6o <34916687+OxheadChuck6o6o@users.noreply.github.com>
2025-02-10 02:04:33 +0000

    visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -119,6 +119,26 @@ def visualisation_tab():
                 dfm2['Start Time'] = pd.NaT
                 dfm2['End Time'] = pd.NaT
 
+                dfm2 = dfm2.groupby('UniqueID',as_index=False).agg({
+                    'Sr. No':'first',
+                    'Product Name':'first',
+                    'Order Processing Date':'first',
+                    'Promised Delivery Date':'first',
+                    'Quantity Required':"sum",
+                    'Components':'first',
+                    'Operation':'first',
+                    'Process Type':'first',
+                    'Machine Number':'first',
+                    'Run Time (min/1000)':'first',
+                    'Cycle Time (seconds)':'first',
+                    'Setup time (seconds)':'first',
+                    'Start Time':'first',
+                    'End Time':'first',
+                    'status':'first',
+                    # 'Status':'first',
+                    'legend':'first'
+                    })
+
                 # Reschedule using the existing state
                 dfm2 = reschedule_production_with_days(dfm2, st.session_state.machine_last_end,
                                                       st.session_state.machine_schedule, dfm1)
